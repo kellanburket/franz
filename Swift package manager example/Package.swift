c032761b20f252d15129885aa3d51407e245d635
luke <luke_lau@icloud.com>
2017-07-02 04:29:51 +0100

    Update example dependency version

--- Swift package manager example/Package.swift
+++ Swift package manager example/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Franz Example",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "../", from: "0.1.0"),
+         .package(url: "../", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
