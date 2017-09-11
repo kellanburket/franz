5e8ae671cedf3cbfa6de6829afa3aee43d71c5bd
luke <luke_lau@icloud.com>
2017-09-10 23:19:17 -0400

    Update swift package manager example to be command line

--- Swift package manager example/Package.swift
+++ Swift package manager example/Package.swift
@@ -4,17 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "Franz Example",
+    name: "FranzExample",
+		products: [
+        .executable(name: "FranzExample", targets: ["FranzExample"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "../", from: "0.2.0"),
+        .package(url: "../", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Franz Example",
-            dependencies: [],
-						path: "Franz Example"),
+            name: "FranzExample",
+            dependencies: ["Franz"])
     ]
 )
---
+++ Swift package manager example/Sources/FranzExample/main.swift
@@ -0,0 +1,13 @@
+import Foundation
+import Franz
+
+let cluster = Cluster(brokers: [("localhost", 9092)], clientId: "FranzExample")
+
+let consumer = cluster.getConsumer(topics: ["test"], groupId: "group")
+consumer.listen { message in
+	print(String(data: message.value, encoding: .utf8)!)
+}
+
+cluster.sendMessage("test", message: "Hello world!")
+
+RunLoop.main.run()
