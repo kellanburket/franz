f8328e08c3577ca94c46c1f70979725319179bc7
luke <luke_lau@icloud.com>
2018-04-16 02:55:37 -0400

    Add some documentation

--- Sources/Franz/KafkaRequest.swift
+++ Sources/Franz/KafkaRequest.swift
@@ -8,10 +8,20 @@
 
 import Foundation
 
+/// A Kafka API request.
 protocol KafkaRequest {
+	/// The API key to represent the type of request.
+	///
+	/// https://kafka.apache.org/protocol.html#protocol_api_keys
 	var apiKey: ApiKey { get }
+	
+	/// The API version. Defaults to 0.
 	var apiVersion: ApiVersion { get }
+	
+	/// The content of the request.
 	var value: KafkaType? { get }
+	
+	/// The type of response you expect to receive when making this request.
 	associatedtype Response: KafkaResponse
 }
 
@@ -19,6 +29,8 @@ extension KafkaRequest {
 	
 	var apiVersion: ApiVersion { return .defaultVersion }
 	
+	/// The request header and content data.
+	/// - Parameter correlationId: A unique correlation ID to associate with the response.
 	func data(correlationId: Int32, clientId: String) -> Data {
 		let content: [KafkaType?] = [apiKey, apiVersion, correlationId, clientId, value]
 		let size: Int32 = content
